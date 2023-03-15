5efa82d9c2e3a1ae79e505229f3dddddeaf12e9c
Benjamin <bdvllrs@gmail.com>
2023-03-15 15:30:03 +0100

    Fix test simple job.

--- tests/test_simple_job.py
+++ tests/test_simple_job.py
@@ -5,29 +5,33 @@ from tests.utils import mock_for_tests
 
 
 @mock.patch("auto_sbatch.sbatch.Popen")
-def test_start_simple_batch(capsys, p_open):
+def test_start_simple_batch(p_open, capsys):
     mock_for_tests(p_open=p_open)
 
+    slurm_params = {
+        "-J": "job-name",
+        "-N": 1,
+        "--time": "01:00:00"
+    }
+    run_script = "main.py"
+
     auto_sbatch(
-        "python {script_name} {all_params}", {
-            "run_script": "main.py",
-            "slurm": {
-                "-J": "job-name",
-                "-N": 1,
-                "--time": "01:00:00"
-            }
-        }
+        "python {script_name} {all_params}",
+        slurm_params,
+        run_script=run_script,
     )
     captured = capsys.readouterr()
-    assert captured.out == """Running generated SLURM script:
-#!/bin/sh
-#SBATCH -J job-name
-#SBATCH -N 1
-#SBATCH --time=01:00:00
+    expected_output = "#!/bin/sh\n"
+    for key, val in slurm_params.items():
+        if key[1] == "-":  # starts with --
+            expected_output += f"#SBATCH {key}={val}\n"
+        else:
+            expected_output += f"#SBATCH {key} {val}\n"
 
-taskId=0
-python "main.py" "gpus=0" "--run-script=main.py"
-"checkpoints_dir='../../checkpoints/$jobId'"
-Mocked communication output
-Mocked communication error
-"""
+    expected_output += "\n"
+    expected_output += "taskId=0\n"
+    expected_output += f"python {run_script} \n"
+    expected_output += "Mocked communication output\n"
+    expected_output += "Mocked communication error\n"
+
+    assert captured.out == expected_output
