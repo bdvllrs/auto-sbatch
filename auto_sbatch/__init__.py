1e20640ea62cf75889b8e4cef15a15110cfade7e
Benjamin <bdvllrs@gmail.com>
2023-03-16 11:41:02 +0100

    Add import to SlurmScriptParser.

--- auto_sbatch/__init__.py
+++ auto_sbatch/__init__.py
@@ -1,10 +1,12 @@
 from auto_sbatch import processes
 from auto_sbatch.experiment_handler import ExperimentHandler
 from auto_sbatch.sbatch import auto_sbatch, SBatch
+from auto_sbatch.slurm_script import SlurmScriptParser
 
 __all__ = [
     "processes",
     "ExperimentHandler",
     "SBatch",
     "auto_sbatch",
+    "SlurmScriptParser"
 ]
